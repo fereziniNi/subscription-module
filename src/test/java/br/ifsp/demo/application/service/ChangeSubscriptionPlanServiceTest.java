5c1f511279baa5850844e9a9ccd0a113cb0ab8f1
Felipe <felipe.doprado@hotmail.com>
2026-04-10 13:34:41 -0300

    test: add failing test for change plan of suspended subscription

--- src/test/java/br/ifsp/demo/application/service/ChangeSubscriptionPlanServiceTest.java
+++ src/test/java/br/ifsp/demo/application/service/ChangeSubscriptionPlanServiceTest.java
@@ -196,6 +196,33 @@ class ChangeSubscriptionPlanServiceTest {
     }
 
 
+    @Test
+    @Tag("UnitTest")
+    @Tag("TDD")
+    void shouldThrowErrorWhenChangingPlanOfSuspendedSubscription() {
+        SubscriptionRepository subscriptionRepository = mock(SubscriptionRepository.class);
+        ChangeSubscriptionPlanService sut = new ChangeSubscriptionPlanService(subscriptionRepository);
+
+        UUID subscriptionId = UUID.randomUUID();
+        Subscription subscription = new Subscription(
+                UUID.randomUUID(),
+                PlanType.PLUS,
+                BillingCycle.MONTHLY,
+                SubscriptionStatus.SUSPENDED,
+                new BigDecimal("49.90"),
+                new BillingPeriod(LocalDate.now(), LocalDate.now().plusMonths(1))
+        );
+
+        when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.of(subscription));
+
+        assertThatThrownBy(() -> sut.changePlan(subscriptionId, PlanType.PRO))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Suspended subscription");
+
+        verify(subscriptionRepository, never()).save(any());
+    }
+
+
 
 
 }
