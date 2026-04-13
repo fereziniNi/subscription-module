299ae5ff3d1a1cd51a1020936efee16333471c0d
Felipe <felipe.doprado@hotmail.com>
2026-04-13 13:39:31 -0300

    test: add test for yearly invoice generation with annual discount - closes #54

--- src/test/java/br/ifsp/demo/application/service/GenerateInvoiceServiceTest.java
+++ src/test/java/br/ifsp/demo/application/service/GenerateInvoiceServiceTest.java
@@ -51,4 +51,29 @@ class GenerateInvoiceServiceTest {
         assertThat(generatedInvoice.getAmount()).isEqualByComparingTo("29.90");
         verify(invoiceRepository).save(any(Invoice.class));
     }
+    @Test
+    @Tag("UnitTest")
+    @Tag("TDD")
+    void shouldGenerateYearlyInvoiceWithAnnualDiscountForPlusPlan() {
+        UUID subscriptionId = UUID.randomUUID();
+
+        Subscription subscription = new Subscription(
+                UUID.randomUUID(),
+                PlanType.PLUS,
+                BillingCycle.YEARLY,
+                SubscriptionStatus.ACTIVE,
+                new BigDecimal("359.28"),
+                new BillingPeriod(LocalDate.of(2026, 6, 1), LocalDate.of(2027, 6, 1))
+        );
+
+        when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.of(subscription));
+        when(invoiceRepository.existsBySubscriptionIdAndPeriod(subscriptionId, subscription.getBillingPeriod())).thenReturn(false);
+
+        Invoice generatedInvoice = sut.generate(subscriptionId);
+
+        assertThat(generatedInvoice.getAmount()).isEqualByComparingTo("359.28");
+        verify(invoiceRepository).save(any(Invoice.class));
+    }
+
+
 }
