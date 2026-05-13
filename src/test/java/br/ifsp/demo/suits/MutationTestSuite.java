649e50170601fabd3323de3730a457accacef3c1
Felipe <felipe.doprado@hotmail.com>
2026-05-13 10:26:54 -0300

    feat: add mutation test suite

---
+++ src/test/java/br/ifsp/demo/suits/MutationTestSuite.java
@@ -0,0 +1,11 @@
+package br.ifsp.demo.suits;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("br.ifsp.demo")
+@IncludeTags("Mutation")
+public class MutationTestSuite {
+}
