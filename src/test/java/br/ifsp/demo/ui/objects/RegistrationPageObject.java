55a5c85e03c5322059d495096ad745a6bc8719cf
fereziniNi <nikinhaferezini14@gmail.com>
2026-05-22 09:01:44 -0300

    test: add UI validations for registration page elements

--- src/test/java/br/ifsp/demo/ui/objects/RegistrationPageObject.java
+++ src/test/java/br/ifsp/demo/ui/objects/RegistrationPageObject.java
@@ -1,6 +1,7 @@
 package br.ifsp.demo.ui.objects;
 
 import br.ifsp.demo.ui.base.BasePageObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class RegistrationPageObject extends BasePageObject {
@@ -10,5 +11,29 @@ public class RegistrationPageObject extends BasePageObject {
         super(driver);
         if (!PAGE_TITLE.equals(pageTitle()))
             throw new IllegalStateException("Wrong page url: " + driver.getCurrentUrl());
+    }
+
+    public boolean isNameFieldVisible() {
+        return driver.findElement(By.name("name")).isDisplayed();
+    }
+
+    public boolean isLastNameFieldVisible() {
+        return driver.findElement(By.name("lastname")).isDisplayed();
+    }
+
+    public boolean isEmailFieldVisible() {
+        return driver.findElement(By.name("email")).isDisplayed();
+    }
+
+    public boolean isPasswordFieldVisible() {
+        return driver.findElement(By.name("password")).isDisplayed();
+    }
+
+    public boolean isCreateAccountButtonVisible() {
+        return driver.findElement(By.cssSelector("button[type='submit']")).isDisplayed();
+    }
+
+    public boolean isLoginLinkVisible() {
+        return driver.findElement(By.cssSelector("a[href='/login']")).isDisplayed();
     }
 }
--- src/test/java/br/ifsp/demo/ui/test/AuthenticationPageObjectTest.java
+++ src/test/java/br/ifsp/demo/ui/test/AuthenticationPageObjectTest.java
@@ -1,6 +1,12 @@
 package br.ifsp.demo.ui.test;
 
-import br.ifsp.demo.ui.base.BasePageObject;
+
+import br.ifsp.demo.ui.objects.AuthenticationPageObject;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AuthenticationPageObjectTest extends BaseSeleniumTest {
     private static final String URL = "https://subscription-module-seven.vercel.app/login";
@@ -10,5 +16,21 @@ public class AuthenticationPageObjectTest extends BaseSeleniumTest {
         driver.get(URL);
     }
 
+    @Test
+    @DisplayName("Should navigate to registration page by clicking the link")
+    void shouldNavigateToRegistrationPageByClickingTheLink() {
+        var authPage = new AuthenticationPageObject(driver);
+        var registrationPage = authPage.navigateToRegistrationPage();
+
+        final var softly = new SoftAssertions();
+
+        softly.assertThat(registrationPage.pageTitle()).isEqualTo("frontend");
+        softly.assertThat(registrationPage.isNameFieldVisible()).isTrue();
+        softly.assertThat(registrationPage.isLastNameFieldVisible()).isTrue();
+        softly.assertThat(registrationPage.isEmailFieldVisible()).isTrue();
+        softly.assertThat(registrationPage.isPasswordFieldVisible()).isTrue();
+
+        softly.assertAll();
+    }
 
 }
