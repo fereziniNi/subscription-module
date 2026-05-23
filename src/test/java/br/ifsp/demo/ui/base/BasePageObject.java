b52919f95d8b3dc03256cf74113b193f0b7f13bf
fereziniNi <nikinhaferezini14@gmail.com>
2026-05-23 10:34:30 -0300

    config: add wait to functions

--- src/test/java/br/ifsp/demo/ui/base/BasePageObject.java
+++ src/test/java/br/ifsp/demo/ui/base/BasePageObject.java
@@ -1,14 +1,39 @@
 package br.ifsp.demo.ui.base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePageObject {
     protected final WebDriver driver;
+    protected final WebDriverWait wait;
 
-    public BasePageObject(WebDriver driver) {
+    protected BasePageObject(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
+
     public String pageTitle(){
         return driver.getTitle();
+    }
+
+    protected WebElement waitForElement(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+    protected void type(By locator, String text) {
+        waitForElement(locator).sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitForElement(locator).getText();
     }
 }
