c07f9fa8a5b48272d36a380be184eb7e44493b3e
fereziniNi <nikinhaferezini14@gmail.com>
2026-05-23 12:21:44 -0300

    fix: correct class name

--- src/test/java/br/ifsp/demo/ui/base/BasePageObject.java
+++ src/test/java/br/ifsp/demo/ui/base/BasePageObject.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class BasePageObject {
+public abstract class BasePageObject {
     protected final WebDriver driver;
     protected final WebDriverWait wait;
 
