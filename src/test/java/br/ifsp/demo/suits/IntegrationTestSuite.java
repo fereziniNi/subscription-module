fce0666e52186f8f425e65e109a0b7b577f35057
xChristianCRX <riccichristian338@gmail.com>
2026-05-25 08:49:25 -0300

    test: adiciona suite de testes de integração

---
+++ src/test/java/br/ifsp/demo/suits/IntegrationTestSuite.java
@@ -0,0 +1,12 @@
+package br.ifsp.demo.suits;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("br.ifsp.demo")
+@IncludeTags("IntegrationTest")
+public class IntegrationTestSuite {
+
+}
