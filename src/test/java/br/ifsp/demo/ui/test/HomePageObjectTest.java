e3833a7fb08884566f7a3132a76e7e5b87a15fe6
fereziniNi <nikinhaferezini14@gmail.com>
2026-05-26 21:55:18 -0300

    feat: login and navegate

--- src/test/java/br/ifsp/demo/ui/test/HomePageObjectTest.java
+++ src/test/java/br/ifsp/demo/ui/test/HomePageObjectTest.java
@@ -1,15 +1,24 @@
 package br.ifsp.demo.ui.test;
 
 import br.ifsp.demo.ui.base.BaseSeleniumTest;
+import br.ifsp.demo.ui.objects.AuthenticationPageObject;
+import br.ifsp.demo.ui.objects.HomePageObject;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomePageObjectTest extends BaseSeleniumTest {
     private static final String LOGIN_URL = "https://subscription-module-seven.vercel.app/login";
+    private static final String VALID_EMAIL = "teste@teste.com";
+    private static final String VALID_PASSWORD = "teste";
 
     @Override
     public void setInitialPage() {
         driver.get(LOGIN_URL);
     }
+    private HomePageObject loginAndNavigateToHome() {
+        var authPage = new AuthenticationPageObject(driver);
+        return authPage.loginSuccessfully(VALID_EMAIL, VALID_PASSWORD);
+    }
+
 
 }
